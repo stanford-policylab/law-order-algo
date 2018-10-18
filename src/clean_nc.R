ad38a55a6061f554b03fae70eb8d77bc0bd869c5
Amy Shoemaker <amyshoe@stanford.edu>
2018-10-18 16:51:03 -0700

    Switch to new opp data, all depts besides state patrol

--- src/clean_nc.R
+++ src/clean_nc.R
@@ -4,43 +4,67 @@
 library(tidyverse)
 library(lubridate)
 
-SOURCE_RDS <- "../data/nc_full.rds"
+# SOURCE_RDS <- "../data/nc_full.rds"
+SOURCE_RDS <- "/share/data/opp/data/states/nc/statewide/clean/statewide.rds"
 TARGET_RDS <- "../data/nc_sample.rds"
 SAMPLE_SIZE <- 1e6
 
 # Read data ---------------------------------------------------------------
-# Original data available from
-# https://openpolicing.stanford.edu/data/
-full_df <- read_rds(SOURCE_RDS)
+full_df <- read_rds(SOURCE_RDS)$data
 
 cat(sprintf("Starting with %.0f rows\n", nrow(full_df)))
 
 # Process and clean -------------------------------------------------------
 keep_cols <- c(
     # Stop characteristics
-    "id", "officer_id",
-    "stop_date", "stop_time", 
-    "police_department", "county_name", 
-    "violation", "stop_outcome",
+    "date", "time", 
+    "department_name",
+    "reason_for_stop",
 
     # Driver demographics
-    "driver_race", "driver_age", "driver_gender",
+    "subject_race", "subject_age", "subject_sex",
     
     # Search reasons
     "search_conducted",
-    "search_type", "search_basis",
+    "reason_for_search", "search_basis",
 
     # Search outcomes
-    "contraband_found"
+    "contraband_found",
+    
+    # Stop outcomes
+    "arrest_made", "citation_issued", "warning_issued"
 )
 
-clean_df <- full_df %>%
+clean_df <- 
+  full_df %>%
   select(!!!keep_cols) %>%
-  filter(
-    police_department == "NC State Highway Patrol",
-    year(stop_date) >= 2008,
-    !is.na(county_name)
+  filter(year(date) >= 2009, year(date) <= 2015) %>%
+  mutate(subject_race = as.character(
+    plyr::mapvalues(
+      subject_race, 
+      from = c("asian/pacific islander", "other/unknown"), 
+      to = c("asian", "other")
+    )
+  )) %>%
+  filter(subject_race != "other") %>%
+  rename(
+    driver_race = subject_race,
+    driver_gender = subject_sex,
+    driver_age = subject_age,
+    police_department = department_name
   )
+
+# Use the top 100 most active police departments (not state patrol)
+active_departments <-
+  clean_df %>%
+  filter(police_department != "NC State Highway Patrol") %>%
+  count(police_department, sort = TRUE) %>%
+  mutate(rank = 1:n()) %>%
+  filter(rank <= 100) %>%
+  pull(police_department)
+clean_df <-
+  clean_df %>%
+  filter(police_department %in% active_departments)
 
 # Sample and save ---------------------------------------------------------
 cat(sprintf("Sampling %d rows\n", SAMPLE_SIZE))
@@ -49,5 +73,5 @@ sample_df <- clean_df %>%
   sample_n(SAMPLE_SIZE)
 
 # Save data
-message("\nSaving clean data file to:", TARGET_RDS)
+message("\nSaving clean data file to: ", TARGET_RDS)
 write_rds(sample_df, TARGET_RDS)
