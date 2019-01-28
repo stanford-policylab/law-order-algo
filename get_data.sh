be19ebc9b1067f3db6644ec988c6c3d1b1e7cec6
Jongbin Jung <me@jongbin.com>
2019-01-28 15:11:57 -0800

    Add script to download sqf_sample data

---
+++ get_data.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Created: 2019-01-28
+# Description: Download data necessary for notebooks
+
+mkdir -p data
+curl -L -o data/sqf_sample.rds "https://drive.google.com/uc?export=download&id=1yW-l-3atdKphyPlPPjZGDO9VaHMfwMh4"
