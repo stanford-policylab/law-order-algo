bb22370a1e4777d4c302b7dde94fec7e923957bb
Jongbin Jung <me@jongbin.com>
2019-03-29 14:40:02 -0700

    Update get data

--- get_data.sh
+++ get_data.sh
@@ -6,3 +6,4 @@
 mkdir -p data
 curl -L -o data/sqf_sample.rds "https://drive.google.com/uc?export=download&id=1yW-l-3atdKphyPlPPjZGDO9VaHMfwMh4"
 curl -L -o data/bar_passage_data.csv "https://drive.google.com/uc?export=download&id=1mOE1s2MFxReEHqVmR829mUpfKzJS7Com"
+curl -L -o data/kc_inspections.rds "https://drive.google.com/uc?export=download&id=1JnwWB9oh8Jw5dfh4HJfm3MXfegZW3tQC"
