06a6d4233f3ef988d0dbbc343323d5c008863bb8
Reid Whitaker <reidmwhitaker@gmail.com>
2019-03-29 11:43:08 -0700

    updated bar passage notebook

--- get_data.sh
+++ get_data.sh
@@ -5,3 +5,4 @@
 
 mkdir -p data
 curl -L -o data/sqf_sample.rds "https://drive.google.com/uc?export=download&id=1yW-l-3atdKphyPlPPjZGDO9VaHMfwMh4"
+curl -L -o data/bar_passage_data.csv "https://drive.google.com/uc?export=download&id=1mOE1s2MFxReEHqVmR829mUpfKzJS7Com"
