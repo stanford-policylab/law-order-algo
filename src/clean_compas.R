7f405df3e5fad9089b0b7ec029307cfc8baea98e
Jongbin Jung <me@jongbin.com>
2019-04-30 10:00:10 -0700

    Update COMPAS data

--- src/clean_compas.R
+++ src/clean_compas.R
@@ -4,41 +4,27 @@
 library(tidyverse)
 
 
-SOURCE_CSV <- "../data/compas_scores_raw.csv"
+# Original, full COMPAS data from ProPublica
+SOURCE_CSV <- "https://github.com/propublica/compas-analysis/raw/master/compas-scores-two-years.csv"
 TARGET_RDS <- "../data/compas.rds"
 
 # Read data ---------------------------------------------------------------
-# Original, full data frame, available from
-# https://raw.githubusercontent.com/propublica/compas-analysis/master/compas-scores-raw.csv
 origin_df <- read_csv(SOURCE_CSV)
 
 cat(sprintf("Starting with %.0f rows\n", nrow(origin_df)))
 
 # Rename/normalize columns ------------------------------------------------
 cn <- colnames(origin_df)
-new_cn <- gsub("\\.", "_", cn)
-new_cn <- tolower(new_cn)
-
-names(origin_df) <- new_cn
-
-map_score_type <- c(
-    `Risk of Violence` = "violence",
-    `Risk of Recidivism` = "recidivism",
-    `Risk of Failure to Appear` = "fta"
-)
 
 # Process and clean -------------------------------------------------------
 clean_df <- origin_df %>%
-  filter(scaleset == "Risk and Prescreen") %>%
-  mutate(score_type = map_score_type[displaytext]) %>%
-  select(person_id,
-         assessment_id = assessmentid,
-         case_id,
-         dob = dateofbirth,
-         sex = sex_code_text,
-         score_type, decilescore) %>%
-  spread(score_type, decilescore)
-
+  filter(race %in% c("Caucasian", "African-American")) %>%
+  select(id, sex, dob, age, race,
+         recid_score = decile_score,
+         violence_score = v_decile_score,
+         is_recid,
+         is_violent_recid) %>%
+  filter(complete.cases(.))
 
 # Save --------------------------------------------------------------------
 # Save data
