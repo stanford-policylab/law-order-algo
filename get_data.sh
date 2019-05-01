1562d90f08f60f5c83ff9691774a195ed03ff66e
itsmrlin <jerrylin1206@gmail.com>
2019-04-30 18:39:53 -0700

    update data script to incorporate new inspection data

--- get_data.sh
+++ get_data.sh
@@ -5,4 +5,5 @@ mkdir -p data
 curl -L -o data/sqf_sample.rds "https://drive.google.com/uc?export=download&id=1yW-l-3atdKphyPlPPjZGDO9VaHMfwMh4"
 curl -L -o data/bar_passage_data.csv "https://drive.google.com/uc?export=download&id=1F93eEZlZqJL7yYKy2YSWvPdLWQbgKUs7"
 curl -L -o data/kc_inspections.rds "https://drive.google.com/uc?export=download&id=1JnwWB9oh8Jw5dfh4HJfm3MXfegZW3tQC"
+curl -L -o data/inspections.rds "https://drive.google.com/uc?export=download&id=1PnZj2TisPYhDEL8wbAeX3qFQ-fPej1_Z"
 curl -L -o data/compas.rds "https://drive.google.com/uc?export=download&id=1sfaE9KxAeHPisaOtkwXOlN5YNy8jrrs5"
